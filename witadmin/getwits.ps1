0825f077a412ddde3c9852e1f45142bc592587a0
Mario Dietner <mario.dietner@gmail.com>
2020-03-19 11:01:55 +0100

    added getwits - exports all wit from azure devops for a given project

---
+++ witadmin/getwits.ps1
@@ -0,0 +1,12 @@
+﻿$pathToIDE = ""
+$collection = ""
+$project = ""
+$path = ""
+
+cd $pathToIDE
+
+$wits = .\witadmin listwitd /collection:$collection /p:$project
+Foreach ($wit IN $wits)
+{
+    $wits = .\witadmin exportwitd  /collection:$collection /p:$project /n:$wit /f:"$path\$($wit).xml"
+}
